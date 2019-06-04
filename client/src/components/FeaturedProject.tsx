f7dde539e401bfcf1b0d3fa6994c49433ba42ded
Nicholas Morrow <njmorrow95@gmail.com>
2019-06-04 19:20:00 -0400

    Able to get fluid image working with StaticQuery

--- client/src/components/FeaturedProject.tsx
+++ client/src/components/FeaturedProject.tsx
@@ -1,6 +1,22 @@
 import * as React from 'react'
 import { Project } from '../types';
+import { graphql, useStaticQuery, StaticQuery } from "gatsby"
+import Img from "gatsby-image"
 
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "cityscape.jpeg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+  `;
 export const FeaturedProject : React.FC<{project: Project}> = ({project}) => {
-	return <div>{project.name}</div>
+	
+	return <StaticQuery query={query} render={data => <Img fluid={data.file.childImageSharp.fluid} />} />
 }
