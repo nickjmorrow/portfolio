caa94b8ac0fd29d58a3c4bc6c3cced042c217e36
Nicholas Morrow <njmorrow95@gmail.com>
2022-03-06 14:03:55 -0600

    Handle Nextjs image export issue.

--- modules/about/Profile.tsx
+++ modules/about/Profile.tsx
@@ -1,10 +1,8 @@
 // external
-import Image from 'next/image';
 import React from "react";
-import profilePic from '../../public/images/profile_picture.jpeg';
 
 export const Profile: React.FC = () => {
   return <div style={{ width: '250px', height: '250px' }}>
-    <Image src={profilePic} />
+    <img src={'/images/profile_picture.jpeg'} />
   </div>;
 };
